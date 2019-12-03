a834d44c412e9ddbcbe07afd0e9f93a7e480654c
Paulin Caratini <p.caratini@outlook.fr>
2019-12-03 23:38:19 +0000

    Update CSS composant experience-preview

--- src/app/shared/experience-card/experience-card.component.ts
+++ src/app/shared/experience-card/experience-card.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
 import { Experience } from 'src/app/core/experience/experience';
 
 @Component({
@@ -8,9 +10,13 @@ import { Experience } from 'src/app/core/experience/experience';
 })
 export class ExperienceCardComponent implements OnInit {
   @Input() experience: Experience;
-  constructor() { }
+  constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
+  }
+
+  getBackgroundImageUrl() {
+    return this.sanitizer.bypassSecurityTrustStyle(`url('${this.experience.cover_picture_url}')`);
   }
 
 }
