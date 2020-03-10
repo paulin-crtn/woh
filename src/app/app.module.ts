eb1e51e5424d8bf1ba4979a822f965b6cc946f86
Paulin Caratini <p.caratini@outlook.fr>
2020-03-10 18:32:58 +0100

    Intégration page Become a host

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -42,7 +43,8 @@ import { CallToActionCardComponent } from './shared/call-to-action-card/call-to-
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/become-host/become-host.component.ts
+++ src/app/components/become-host/become-host.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-become-host',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./become-host.component.scss']
 })
 export class BecomeHostComponent implements OnInit {
+  registerForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private titleService: Title,
+    private meta: Meta,
+    private fb: FormBuilder,
+  ) { }
 
   ngOnInit() {
+      // PAGE TITLE
+      this.titleService.setTitle('Become a host | Worldhelpers');
+      // META DESCRIPTION
+      this.meta.updateTag({name: 'description', content: ''});
+      // FORM
+      this.registerForm = this.fb.group({
+        firstname: this.fb.control(''),
+        lastname: this.fb.control(''),
+        email: this.fb.control(''),
+        password: this.fb.control(''),
+        gdpr: this.fb.control(''),
+      });
   }
+
+
 
 }
