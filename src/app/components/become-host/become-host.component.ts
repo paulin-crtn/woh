7ce8160fcd05ec3d7f64b218936ab2887c1b3d3d
Paulin Caratini <p.caratini@outlook.fr>
2020-03-11 12:35:54 +0100

    Page Become a host : form validation & loader button

--- src/app/components/become-host/become-host.component.ts
+++ src/app/components/become-host/become-host.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
@@ -9,6 +9,7 @@ import { Title, Meta } from '@angular/platform-browser';
 })
 export class BecomeHostComponent implements OnInit {
   registerForm: FormGroup;
+  submitted: Boolean;
 
   constructor(
     private titleService: Title,
@@ -23,14 +24,23 @@ export class BecomeHostComponent implements OnInit {
       this.meta.updateTag({name: 'description', content: ''});
       // FORM
       this.registerForm = this.fb.group({
-        firstname: this.fb.control(''),
-        lastname: this.fb.control(''),
-        email: this.fb.control(''),
-        password: this.fb.control(''),
-        gdpr: this.fb.control(''),
+        firstname: this.fb.control('', [Validators.required, Validators.minLength(2), Validators.maxLength(100)]),
+        lastname: this.fb.control('', [Validators.required, Validators.minLength(2), Validators.maxLength(100)]),
+        email: this.fb.control('', [Validators.required, Validators.email]),
+        password: this.fb.control('', [Validators.required, Validators.minLength(6)]),
+        consent: this.fb.control('', Validators.requiredTrue),
       });
   }
 
+  // SHORTCUT TO DISPLAY FORM ERROR MESSAGES IN THE TEMPLATE
+  get firstname() { return this.registerForm.get('firstname'); }
+  get lastname() { return this.registerForm.get('lastname'); }
+  get email() { return this.registerForm.get('email'); }
+  get password() { return this.registerForm.get('password'); }
 
+  register() {
+    console.log(this.registerForm.value);
+    this.submitted = true;
+  }
 
 }
