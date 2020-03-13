ed682fdb70de887cd15a9fb17c7864caaef238ad
Paulin Caratini <p.caratini@outlook.fr>
2020-03-13 18:52:37 +0100

    Revert "Revert "Finalisation partie helper page how it works""
    This reverts commit 1853e840f772f62c70038572f50cb056bb2a6d15.

--- src/app/components/how-it-works/how-it-works.component.ts
+++ src/app/components/how-it-works/how-it-works.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-how-it-works',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HowItWorksComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private titleService: Title,
+    private meta: Meta,
+  ) { }
 
   ngOnInit() {
+    // PAGE TITLE
+    this.titleService.setTitle('How it works | Worldhelpers');
+    // META DESCRIPTION
+    this.meta.updateTag({name: 'description', content: ''});
   }
 
 }
