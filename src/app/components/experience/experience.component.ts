46b4586eff92d5dce2fb984192d998602eafe139
Paulin Caratini <p.caratini@outlook.fr>
2020-03-18 18:24:39 +0100

    Finalisation galerie images page Experience + bug fix

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Title, Meta, DomSanitizer } from '@angular/platform-browser';
 
 import { Experience } from 'src/app/core/experience/experience';
 import { ExperienceMockService } from 'src/app/core/experience/experience.mock.service';
+
+import { NgxImageGalleryComponent, GALLERY_IMAGE, GALLERY_CONF } from "ngx-image-gallery";
 
 @Component({
   selector: 'app-experience',
@@ -10,6 +12,26 @@ import { ExperienceMockService } from 'src/app/core/experience/experience.mock.s
   styleUrls: ['./experience.component.scss']
 })
 export class ExperienceComponent implements OnInit {
+
+  /**************************************************/
+  // GALLERY INIT - Source https://www.npmjs.com/package/ngx-image-gallery-public
+  /**************************************************/
+
+  // Get reference to gallery component
+  @ViewChild(NgxImageGalleryComponent, null) ngxImageGallery: NgxImageGalleryComponent;
+  
+  // Gallery configuration
+  conf: GALLERY_CONF = {
+    imageBorderRadius: '10px',
+    imageOffset: '20px',
+    thumbnailSize: 90,
+    showImageTitle: false,
+  };
+	
+  // Gallery images
+  images: GALLERY_IMAGE[] = [];
+
+  /**************************************************/
 
   experience: Experience;
 
@@ -29,15 +51,42 @@ export class ExperienceComponent implements OnInit {
       this.experience = experience;
       console.log(this.experience);
     });
-
+    // FILL GALLERY_IMAGE
+    this.fillGallery();
   }
 
-  displayGallery() {
+  /**************************************************/
+  // GALLERY METHODS
+  /**************************************************/
+
+  // Open gallery
+  openGallery(index: number = 0) {
+    this.ngxImageGallery.open(index);
+  }
+	
+  // Close gallery
+  closeGallery() {
+    this.ngxImageGallery.close();
+  }
+	
+  // Next image in gallery
+  nextImage(index: number = 0) {
+    this.ngxImageGallery.next();
+  }
+	
+  // Prev image in gallery
+  prevImage(index: number = 0) {
+    this.ngxImageGallery.prev();
+  }
+	
+  /**************************************************/
+  
+  fillGallery() {
     for (const url of this.experience.gallery_pictures_url) {
-      console.log(url);
+      this.images.push({url});
     }
   }
-  
+
   getBackgroundImageUrl() {
     return this.sanitizer.bypassSecurityTrustStyle(`url('${this.experience.gallery_pictures_url[0]}')`);
   }
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import 'hammerjs'; // Prerequis ngx-image-gallery 
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
